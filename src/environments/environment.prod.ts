8be11c3e480fc43bc96c03f53211250a1f616c48
jsales3 <jovonsales@gmail.com>
2019-01-31 22:32:49 -0500

    first commit/completed section 1 of app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBwFJAjN78GsvlBpNsE17M24Z_4C6faKbg",
+    authDomain: "oshop-4532a.firebaseapp.com",
+    databaseURL: "https://oshop-4532a.firebaseio.com",
+    projectId: "oshop-4532a",
+    storageBucket: "oshop-4532a.appspot.com",
+    messagingSenderId: "483742828723"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBwFJAjN78GsvlBpNsE17M24Z_4C6faKbg",
+    authDomain: "oshop-4532a.firebaseapp.com",
+    databaseURL: "https://oshop-4532a.firebaseio.com",
+    projectId: "oshop-4532a",
+    storageBucket: "oshop-4532a.appspot.com",
+    messagingSenderId: "483742828723"
+  }
 };
 
 /*
